ffc5ba6fc960e7f56ede34371b31b0d61f4fa456
Ian Clarke <ian.clarke@gmail.com>
2013-12-12 09:40:46 -0600

    deployed by script

--- redspot/redspot.js
+++ redspot/redspot.js
@@ -1,9 +1,32 @@
 // Put event listeners into place
+
+function findPos(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+}
+
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+function drawVideo() {
+	$("#canvas-orig")[0].drawImage($('#webcam')[0], 0, 0, $('#webcam')[0].width, $('#webcam')[0].height);
+}
+
 window.addEventListener("DOMContentLoaded", function() {
 	// Grab elements, create settings, etc.
 	var canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
-		video = document.getElementById("video"),
+		video = document.getElementById("webcam"),
 		videoObj = { "video": true },
 		errBack = function(error) {
 			console.log("Video capture error: ", error.code);
@@ -21,4 +44,7 @@ window.addEventListener("DOMContentLoaded", function() {
 			video.play();
 		}, errBack);
 	}
+
+
 }, false);
+
